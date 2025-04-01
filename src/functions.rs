dd516fe1355938884868b9af56a32c612291bb96
Joseph Guhlin <joseph.guhlin@gmail.com>
2025-04-02 11:19:56 +1300

    Annotate ancestral alleles

--- src/functions.rs
+++ src/functions.rs
@@ -1,5 +1,7 @@
+mod annotate_ancestral_allele;
 mod extract;
 mod remove_ref_indels;
 
+pub use annotate_ancestral_allele::annotate_ancestral_allele;
 pub use extract::extract_snps;
 pub use remove_ref_indels::remove_ref_indels;
--- src/parsers.rs
+++ src/parsers.rs
@@ -1,3 +1,7 @@
 mod maf;
+mod taffy;
+mod vcf42;
 
 pub use maf::*;
+pub use taffy::*;
+pub use vcf42::*;
